3a7b6bd3bb201859e4985410c69e30d1bf062254
Bijalkovic <bijalkovicvanja@gmail.com>
2021-06-03 19:09:48 +0200

    Delete emlty rows

--- config/app.php
+++ config/app.php
@@ -229,8 +229,6 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class,
-        
-
     ],
 
 ];
